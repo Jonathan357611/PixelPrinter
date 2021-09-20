93305d07ddc30abaea22cc0e1be687854dbccfe1
Jonathan357611 <jonathan.fortmann@gmail.com>
2021-09-20 18:35:26 +0200

    3rd commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+
+RESET = '\033[0m'
+def get_color_escape(r, g, b, background=False):
+    return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
+
+
+def get_image_color(filename):
+    im = Image.open(filename, 'r')
+    width, height = im.size
+    pixel_values = list(im.getdata())
+    # print(len(pixel_values))
+
+    for i, color in enumerate(pixel_values):
+        print(get_color_escape(color[0], color[1], color[2]) + get_color_escape(color[0], color[1], color[2], True) + "██", end="")
+        if i % 88 == 0:
+            ...
+            # print("\n")
+
+
+
+get_image_color("image.png")
+
+#print(get_color_escape(255, 128, 0) + '████\n████'
+#      + RESET)
