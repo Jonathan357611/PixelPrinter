00d7fb71651065fdc31d3549f9e3082618d249a1
Jonathan357611 <jonathan.fortmann@gmail.com>
2021-09-20 20:33:58 +0200

    added error-handling

--- cli_test.py
+++ cli_test.py
@@ -8,7 +8,7 @@ def error_msg(msg):
 @click.option("--image", "-i", "image", required=True, help="Path to image to display")
 @click.option("--downscale", "-d", "downscale", default="1", help="Downscale displayed image by n")
 def process(image, downscale):
-    if not downscale.is_numeric:
+    if not downscale.isnumeric:
         print(error_msg("--downscale is not int!"))
     test.show(image, int(downscale))
 
