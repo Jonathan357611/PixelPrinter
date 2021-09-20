1de7d0d9bb2d4edccabf7c9ba11c9b8db8da0afd
Jonathan357611 <jonathan.fortmann@gmail.com>
2021-09-20 19:40:44 +0200

    fix

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def get_image_color(filename):
     pixel_values = list(im.getdata())
 
     for i, color in enumerate(pixel_values):
-        print(get_color_escape(color[0], color[1], color[2], True) + f"  {RESET}", end="")
         if i % width == 0:
             print("")
+        print(get_color_escape(color[0], color[1], color[2], True) + f"  {RESET}", end="")
+
     print("\n")
 
 get_image_color(input("file (test) > "))
