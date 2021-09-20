69f0ffaf112ed28da0b7db3cb6201206537e0fba
Jonathan357611 <jonathan.fortmann@gmail.com>
2021-09-20 20:22:31 +0200

    fix

--- cli_test.py
+++ cli_test.py
@@ -3,7 +3,7 @@ import main as test
 
 @click.command()
 @click.option("--image", "-i", "image", required=True, help="Path to image to display")
-@click.option("--downscale", "-d", "downscale", default="0", help="Downscale displayed image by n")
+@click.option("--downscale", "-d", "downscale", default="1", help="Downscale displayed image by n")
 def process(image, downscale):
     test.show(image, int(downscale))
 
