6cbbbd047b7f90e296c9703f2c843d4520062ba8
Jonathan357611 <jonathan.fortmann@gmail.com>
2021-09-21 21:27:56 +0200

    Testing optimization

--- PixelPrinter.py
+++ PixelPrinter.py
@@ -49,7 +49,6 @@ class PixelPrinter:
                 pass
         print("\n")
 
-
 @click.command()
 @click.option("--image", "-i", "image", required=True, help="Path to image to display")
 @click.option("--downscale", "-d", "downscale", default=1, help="Downscale displayed image by n")
